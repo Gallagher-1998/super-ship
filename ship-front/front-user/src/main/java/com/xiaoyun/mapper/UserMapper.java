018a49fb6fec0f294b5c5c097f1d061c5542f8dc
gallagher <867065336@qq.com>
2020-01-14 21:37:54 +0800

    前台会员注册

--- ship-front/front-user/src/main/java/com/xiaoyun/mapper/UserMapper.java
+++ ship-front/front-user/src/main/java/com/xiaoyun/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package com.xiaoyun.mapper;
 
+import com.xiaoyun.pojo.UserPojo;
+
 public interface UserMapper {
+
+    public String checkName(String vName);
+
+    public boolean register(UserPojo userPojo);
 }
--- ship-front/front-user/src/main/java/com/xiaoyun/service/IUserService.java
+++ ship-front/front-user/src/main/java/com/xiaoyun/service/IUserService.java
@@ -1,4 +1,10 @@
 package com.xiaoyun.service;
 
+import com.xiaoyun.pojo.UserPojo;
+
 public interface IUserService {
+
+    public String checkName(String vName);
+
+    public boolean register(UserPojo userPojo);
 }
